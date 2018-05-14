9cd626709897f3d28656bca9f08adafb48725b51
1c7 <guokrfans@gmail.com>
2018-05-14 18:17:59 +0800

    simplify

--- third_party_lib/upload.js
+++ third_party_lib/upload.js
@@ -10,14 +10,13 @@ export default function upload(file_name, file_type, file_path) {
   var accessid = 'LTAIpUOrlri12345';
   var accesskey = '4ifvcEGWMwobGvQWM3c4hefT123456';
 
-  // 计算签名和其他参数
+  // 算签名和其他参数
   var policyText = {
     "expiration": "2020-01-01T12:00:00.000Z", // 设置该Policy的失效时间，超过这个失效时间之后，就没有办法通过这个policy上传文件了
     "conditions": [
       ["content-length-range", 0, 1048576000] // 设置上传文件的大小限制
     ]
   };
-
   var policyBase64 = Base64.encode(JSON.stringify(policyText))
   var message = policyBase64
   var bytes = Crypto.HMAC(Crypto.SHA1, message, accesskey, {
@@ -25,8 +24,9 @@ export default function upload(file_name, file_type, file_path) {
   });
   var signature = Crypto.util.bytesToBase64(bytes);
 
-  // 发送请求
-  var url = 'https://xxxxx-upload.oss-cn-shenzhen.aliyuncs.com'; // 【换成你自己的】
+  // 发请求
+  var url = 'https://xxxxx-upload.oss-cn-shenzhen.aliyuncs.com'; 
+  // url【换成你自己的】
   return RNFetchBlob.fetch('POST', url, {}, [
     { name: 'key', data: file_name },
     { name: 'policy', data: policyBase64 },
